0824dbc33df15eacc299cb25782f08367aa91e0e
JaeHoonLee <ljhgood9925@gmail.com>
2018-10-04 04:49:28 +0900

    Initial commit

---
+++ app/src/main/java/com/edustudy/android/jdfree/jjandroidedustudy/MainActivity.java
@@ -0,0 +1,13 @@
+package com.edustudy.android.jdfree.jjandroidedustudy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
