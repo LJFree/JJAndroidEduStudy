451e584e8cad697760c8ceb863edcf716104c7a8
JaeHoonLee <ljhgood9925@gmail.com>
2018-10-04 04:58:12 +0900

    기초1 : Main2Activity 로 화면 전환

--- app/src/main/java/com/edustudy/android/jdfree/jjandroidedustudy/MainActivity.java
+++ app/src/main/java/com/edustudy/android/jdfree/jjandroidedustudy/MainActivity.java
@@ -1,7 +1,9 @@
 package com.edustudy.android.jdfree.jjandroidedustudy;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void showActivity(View view) {
+        // Main2Activity 로 화면 전환
+        Intent intent = new Intent(this, Main2Activity.class);
+        startActivity(intent);
     }
 }
