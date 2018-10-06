cb465441efe76dbf9964af1fbcec3f7dff08a33e
jaehoonLee <ljhgood9925@gmail.com>
2018-10-06 13:30:13 +0900

    기초5 : ListView 종류 - 리스트 뷰 - 사용자 어뎁터 - 뷰 적용

--- chapter4/src/main/java/com/jdfree/jjandroidedustudy/chapter4/ListBaseAdapter.java
+++ chapter4/src/main/java/com/jdfree/jjandroidedustudy/chapter4/ListBaseAdapter.java
@@ -4,12 +4,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.TextView;
 
 import java.util.List;
 
 public class ListBaseAdapter extends BaseAdapter {
 
-    private List mData;
+    private List<InfoModel> mData;
 
     public ListBaseAdapter(List mData) {
         this.mData = mData;
@@ -32,6 +33,18 @@ public class ListBaseAdapter extends BaseAdapter {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        return LayoutInflater.from(parent.getContext()).inflate(R.layout.item_list, parent, false);
+        convertView = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_list, parent, false);
+
+        TextView textView1 = convertView.findViewById(R.id.item_list_textview1);
+        TextView textView2 = convertView.findViewById(R.id.item_list_textview2);
+        TextView textView3 = convertView.findViewById(R.id.item_list_textview3);
+
+        InfoModel model = mData.get(position);
+
+        textView1.setText(model.getField1());
+        textView2.setText(model.getField2());
+        textView3.setText(model.getField3());
+
+        return convertView;
     }
 }
