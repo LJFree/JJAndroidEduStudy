54f68be08e1c9ddaa714b74dffc68be707ad68bb
jaehoonLee <ljhgood9925@gmail.com>
2018-10-06 12:43:53 +0900

    기초5 : ListView 종류 - 리스트 뷰 - 기본 어뎁터

--- chapter4/src/main/java/com/jdfree/jjandroidedustudy/chapter4/MainActivity.java
+++ chapter4/src/main/java/com/jdfree/jjandroidedustudy/chapter4/MainActivity.java
@@ -2,6 +2,13 @@ package com.jdfree.jjandroidedustudy.chapter4;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ArrayList list = new ArrayList();
+
+        for (int i = 0; i < 100; i++) {
+            list.add(i + "번째");
+        }
+
+        final ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, list);
+
+        ListView listView = findViewById(R.id.list_view);
+        listView.setAdapter(adapter);
+
+        // 클릭 이벤트
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                Toast.makeText(MainActivity.this, adapter.getItem(position).toString(), Toast.LENGTH_SHORT).show();
+            //    Toast.makeText(MainActivity.this, parent.getItemAtPosition(position).toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
