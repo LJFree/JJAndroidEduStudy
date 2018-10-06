fc530a94c80489c7bbf5abefbce154ca03339b64
jaehoonLee <ljhgood9925@gmail.com>
2018-10-06 11:22:42 +0900

    기초4 : Viewpager Fragment 연결 PagerAdapter 이용

---
+++ chapter3/src/main/java/com/jdfree/jjandroidedustudy/chapter3/PagerAdapter.java
@@ -0,0 +1,30 @@
+package com.jdfree.jjandroidedustudy.chapter3;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+
+public class PagerAdapter extends FragmentPagerAdapter {
+
+    ArrayList<Fragment> mData;
+
+    public PagerAdapter(FragmentManager fm) {
+        super(fm);
+
+        mData = new ArrayList<>();
+        mData.add(new BlankFragment());
+        mData.add(new BlankFragment2());
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return mData.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return mData.size();
+    }
+}
