3b8d97c303be0f319b4f48da56f1a93535d44163
jaehoonLee <ljhgood9925@gmail.com>
2018-10-10 22:22:25 +0900

    기초6 : AlertDialog.Builder 이용하여 종료 구현하기 기본 - 다른 방식의 코드 1회성일 경우 인스턴스화 생략

--- chapter5/src/main/java/com/firebasechart/jdfree/jjandroidedustudy/chapter5/MainActivity.java
+++ chapter5/src/main/java/com/firebasechart/jdfree/jjandroidedustudy/chapter5/MainActivity.java
@@ -17,12 +17,11 @@ public class MainActivity extends AppCompatActivity {
     /***
      * 기본적으로 AlertDialog 를 사용하게 되면 화면 전환 시 기능이 없어진다. 이를 소멸이라고도 하며, 생명이 죽었다라고 한다. 생명을
      * 주기 위해서는 여러가지 방법이 있다. 1. 화면을 고정시켜 화면 전환이 불가능 하도록 하는 방법 2. Fragment 를 이용하여 생명주는
-     * 방법 다른 방식
+     * 방법 다른 방식 - 1회성일 경우 인스턴스화 생략하여 사용
      */
     @Override
     public void onBackPressed() { // 뒤로가기 누르면 실행되는 이벤트
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle("종료 할래?").setMessage("진짜 종료할꺼야?")
+        new AlertDialog.Builder(this).setTitle("종료 할래?").setMessage("진짜 종료할꺼야?")
                 .setPositiveButton("종료할래", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
