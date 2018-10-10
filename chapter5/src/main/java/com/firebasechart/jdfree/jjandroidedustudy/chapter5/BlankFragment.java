9a9ea1ecc14bc0844e715227c1dd94c9a0955996
jaehoonLee <ljhgood9925@gmail.com>
2018-10-11 00:10:02 +0900

    기초6 : Fragment -> Activity 값 보내기

---
+++ chapter5/src/main/java/com/firebasechart/jdfree/jjandroidedustudy/chapter5/BlankFragment.java
@@ -0,0 +1,36 @@
+
+package com.firebasechart.jdfree.jjandroidedustudy.chapter5;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class BlankFragment extends Fragment {
+
+    private EditText editText;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_blank, container, false);
+
+        editText = view.findViewById(R.id.editTextFragment1);
+
+        return view;
+    }
+
+    public EditText getEditText() {
+        return editText;
+    }
+
+    public void setEditText(EditText editText) {
+        this.editText = editText;
+    }
+}
--- chapter5/src/main/java/com/firebasechart/jdfree/jjandroidedustudy/chapter5/MainActivity.java
+++ chapter5/src/main/java/com/firebasechart/jdfree/jjandroidedustudy/chapter5/MainActivity.java
@@ -3,6 +3,9 @@ package com.firebasechart.jdfree.jjandroidedustudy.chapter5;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void sendButton(View view) {
+        BlankFragment blankFragment = (BlankFragment)getSupportFragmentManager()
+                .findFragmentById(R.id.fragment1);
+
+        EditText fragmentString = blankFragment.getEditText();
+
+        TextView textView = findViewById(R.id.textViewActivity);
+        textView.setText(fragmentString.getText());
     }
 }
