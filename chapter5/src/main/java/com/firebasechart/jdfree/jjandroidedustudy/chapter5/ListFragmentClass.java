9a5b98013369f759f12d6879f6807df612483d55
jaehoonLee <ljhgood9925@gmail.com>
2018-10-10 22:57:19 +0900

    기초6 : ListFragment 이용하여 쉬운 List 형태 만들기 - 클릭 이벤트

--- chapter5/src/main/java/com/firebasechart/jdfree/jjandroidedustudy/chapter5/ListFragmentClass.java
+++ chapter5/src/main/java/com/firebasechart/jdfree/jjandroidedustudy/chapter5/ListFragmentClass.java
@@ -7,11 +7,15 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.ListFragment;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.Arrays;
 import java.util.List;
 
 public class ListFragmentClass extends ListFragment {
+
+    private ArrayAdapter<String> mAdapter;
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
@@ -19,8 +23,15 @@ public class ListFragmentClass extends ListFragment {
 
         List<String> list = Arrays.asList("선택1", "선택2", "선택3");
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(getActivity(),
-                android.R.layout.simple_list_item_1, list);
-        setListAdapter(adapter);
+        mAdapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_list_item_1, list);
+        setListAdapter(mAdapter);
+    }
+
+    @Override
+    public void onListItemClick(ListView l, View v, int position, long id) {
+        String selectItem = mAdapter.getItem(position);
+
+        Toast.makeText(getContext(), selectItem + " 클릭", Toast.LENGTH_SHORT).show();
+
     }
 }
