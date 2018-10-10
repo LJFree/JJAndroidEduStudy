9344b24ed906dc8d01a94ca4c571c36e2651bf21
jaehoonLee <ljhgood9925@gmail.com>
2018-10-10 22:16:43 +0900

    기초6 : AlertDialog.Builder 이용하여 종료 구현하기 기본

--- chapter3/src/main/java/com/jdfree/jjandroidedustudy/chapter3/MainActivity.java
+++ chapter3/src/main/java/com/jdfree/jjandroidedustudy/chapter3/MainActivity.java
@@ -1,11 +1,9 @@
 package com.jdfree.jjandroidedustudy.chapter3;
 
+import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-
-import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ chapter5/src/main/java/com/firebasechart/jdfree/jjandroidedustudy/chapter5/MainActivity.java
@@ -0,0 +1,38 @@
+
+package com.firebasechart.jdfree.jjandroidedustudy.chapter5;
+
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /***
+     * 기본적으로 AlertDialog 를 사용하게 되면 화면 전환 시 기능이 없어진다. 이를 소멸이라고도 하며, 생명이 죽었다라고 한다. 생명을
+     * 주기 위해서는 여러가지 방법이 있다. 1. 화면을 고정시켜 화면 전환이 불가능 하도록 하는 방법 2. Fragment 를 이용하여 생명주는
+     * 방법
+     */
+    @Override
+    public void onBackPressed() { // 뒤로가기 누르면 실행되는 이벤트
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("종료 할래?");
+        builder.setMessage("진짜 종료할꺼야?");
+
+        builder.setPositiveButton("종료할래", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                finish();
+            }
+        });
+
+        builder.setNegativeButton("안할래", null);
+        builder.show();
+    }
+}
